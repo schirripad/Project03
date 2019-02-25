90f720b8774c28dcf644a3b19e3e907d410fc61e
schirripad@moravian.edu <schirripad@moravian.edu>
2019-02-25 17:15:58 -0500

    Fixed to accommadate modifications to Address

--- SandwichTruck.java
+++ SandwichTruck.java
@@ -3,12 +3,13 @@ package Simulation;
 import java.util.PriorityQueue;
 
 public class SandwichTruck {
+	private static final Address distribtutionCenter = new Address(910, 9, StreetDirection.SOUTH);
 
 	public Address curAddress;
 	public PriorityQueue<Address> targets = new PriorityQueue<Address>();
 
-	public SandwichTruck(int east, int south) {
-		curAddress = new Address(east, south);
+	public SandwichTruck(int addressNum, int streetNum, StreetDirection streetDir) {
+		curAddress = new Address(addressNum, streetNum, streetDir);
 	}
 
 	public Address getCurrentAddress() {

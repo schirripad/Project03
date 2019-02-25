5deed316fdc12010e431d154d396b2f3567e3acf
schirripad@moravian.edu <schirripad@moravian.edu>
2019-02-25 17:14:42 -0500

    Modified Address to better model a street address, began work on computing distance between addresses

--- Address.java
+++ Address.java
@@ -2,24 +2,40 @@ package Simulation;
 
 public class Address implements Comparable<Address> {
 
-	public int east;
-	public int south;
+	private int houseNumber, streetNumber;
+	private StreetDirection streetDir;
 
-	public Address(int east, int south) {
-		this.east = east;
-		this.south = south;
+	public Address(int houseNumber, int streetNum, StreetDirection street) {
+		this.houseNumber = houseNumber;
+		this.streetNumber = streetNum;
+		this.streetDir = street;
 	}
 
 	public int distanceFrom(Address a) {
+		// Create address dictionary
+		int hNum = a.getHouseNumber();
+		int sNum = a.getStreetNumber();
+		StreetDirection sDir = a.getStreetDirection();
+		// Compute distance
+		// If houses are on same street, simply subtract housenumbers
+		if (sDir == streetDir && sNum == streetNumber) {
+			return Math.abs(hNum - houseNumber);
+		} else {
+			// Compute distance
+		}
 		return -1;
 	}
 
-	public int getEast() {
-		return east;
+	public int getHouseNumber() {
+		return houseNumber;
 	}
 
-	public int getSouth() {
-		return south;
+	public int getStreetNumber() {
+		return streetNumber;
+	}
+
+	public StreetDirection getStreetDirection() {
+		return streetDir;
 	}
 
 	@Override

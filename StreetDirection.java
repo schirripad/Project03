155ff569616a1b147e4d3c744538c04455586bf7
schirripad@moravian.edu <schirripad@moravian.edu>
2019-02-25 17:16:38 -0500

    Added StreetDirection enum to help specify Address

---
+++ StreetDirection.java
@@ -0,0 +1,11 @@
+package Simulation;
+
+/**
+ * Streets, representative of their respective titles, SOUTH, EAST
+ * 
+ * @author schirripad@moravian.edu
+ *
+ */
+public enum StreetDirection {
+	SOUTH, EAST
+}

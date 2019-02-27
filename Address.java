427b2eb19cdd7f53e67f9de5a3866b1bcd867b1c
hutchinsond02@moravian.edu <hutchinsond02@moravian.edu>
2019-02-26 19:09:17 -0500

    Random Address Generator v1, 100 lines outputted to addresses.txt

--- Address.java
+++ Address.java
@@ -1,8 +1,12 @@
 package Simulation;
 
+import java.util.Random;
+import java.util.*;
+import java.io.*;
+
 public class Address implements Comparable<Address> {
 
-	private int houseNumber, streetNumber;
+	private int houseNumber, streetNumber, blockNum, count;
 	private StreetDirection streetDir;
 
 	public Address(int houseNumber, int streetNum, StreetDirection street) {
@@ -10,6 +14,49 @@ public class Address implements Comparable<Address> {
 		this.streetNumber = streetNum;
 		this.streetDir = street;
 	}
+
+	public void RandomAddress(int houseNumber, int streetNumber, int streetDir) throws IOException{
+        PrintWriter out = new PrintWriter(new File("addresses.txt"));
+        Random rand = new Random();
+
+        while(count<100)
+        {
+            for(int i = 0; i < 100; i++){
+
+                StringBuilder strBuilder = new StringBuilder();
+
+                blockNum = rand.nextInt((100 - 1) + 1 ) + 1;
+                //System.out.println(blockNum);
+                houseNumber = (blockNum * 10);
+                if((houseNumber / 10) != 0 && houseNumber != 1000){
+                    strBuilder.append(houseNumber);
+                }
+
+                streetDir = rand.nextInt((2-1) + 1);
+                //System.out.println(southEastNum);
+                if(streetDir == 0)
+                    strBuilder.append(" South");
+                else
+                    strBuilder.append(" East");
+
+
+
+
+                streetNumber = rand.nextInt((20 - 1) + 1);
+                strBuilder.append(" "+streetNumber+" Street");
+                out.print(strBuilder);
+
+
+
+                count++;
+                out.println();
+
+            }
+
+        }
+        out.close();
+    }
+
 
 	public int distanceFrom(Address a) {
 		// Create address dictionary

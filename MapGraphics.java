811a4e74c2668921f0b8073af443067a722e100a
Tongonto <tongonto@gmail.com>
2019-02-28 08:40:48 -0500

    Brought everything together, fixed bug with AddressLoader.

--- MapGraphics.java
+++ MapGraphics.java
@@ -8,33 +8,48 @@ public class MapGraphics extends Canvas {
 	
 	public SandwichTruck parent;
 	public int size = 400;
-	int scale = 2;
+	double scale = 2.0;
 	
 	public MapGraphics(SandwichTruck truck, int size) {
 		
 		parent = truck;
 		this.size = size;
-		scale = size / 200;
+		scale = size / 2000.0;
 		
 	}
 	
 	public void paint(Graphics g) {
 		
 		g.setColor(Color.BLACK);
-		
+
 		for(int i=0; i<20; i++) {
 			
-			g.drawLine(0, i*10*scale, 400, i*10*scale);
-			g.drawLine(i*10*scale, 0, i*10*scale, 400);
+			g.drawLine(0, (int) (i*100*scale), 400, (int) (i*100*scale));
+			g.drawLine((int) (i*100*scale), 0, (int) (i*100*scale), 400);
 			
 		}
 		
-		g.setColor(Color.RED);
+		g.setColor(Color.BLUE);
 		
-		for(Address a : parent.targets) {
+		Address truckAddress = parent.curAddress;
+		int truckX = (int) (truckAddress.getHouseNumber() * scale);
+		int truckY = (int) (truckAddress.getStreetNumber() * 100 * scale);
+				
+		if(truckAddress.getStreetDirection().equals(StreetDirection.SOUTH)) {
 			
-			int x = a.getHouseNumber() * scale;
-			int y = a.getStreetNumber() * 100 * scale;
+			int save = truckX;
+			truckX = truckY;
+			truckY = save;
+			
+		}
+		
+		g.fillOval(truckX, truckY, 3, 3);
+
+		for(Address a : parent.targets) {
+
+			g.setColor(new Color(255 - (255 * a.distanceFrom(truckAddress)) / 2000, 0, 0));
+			int x = (int) (a.getHouseNumber() * scale);
+			int y = (int) (a.getStreetNumber() * 100 * scale);
 			
 			if(a.getStreetDirection().equals(StreetDirection.SOUTH)) {
 				
@@ -47,22 +62,6 @@ public class MapGraphics extends Canvas {
 			g.fillOval(x, y, 3, 3);
 			
 		}
-		
-		g.setColor(Color.BLUE);
-		
-		Address truckAddress = parent.curAddress;
-		int truckX = truckAddress.getHouseNumber() * scale;
-		int truckY = truckAddress.getStreetNumber() * 100 * scale;
-		
-		if(truckAddress.getStreetDirection().equals(StreetDirection.SOUTH)) {
-			
-			int save = truckX;
-			truckX = truckY;
-			truckY = save;
-			
-		}
-		
-		g.fillOval(truckX, truckY, 3, 3);
 		
 	}
 	

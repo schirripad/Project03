f8e43b616f8adb8ee243eaef2e778a8f1fb00d30
riyadalghamdi <alghamdir@moravian.edu>
2019-05-01 14:44:31 -0400

    add askUser method to ask the user which strategy will be used.

--- gui/MapWindow.java
+++ gui/MapWindow.java
@@ -1,11 +1,11 @@
 package Simulation.gui;
 
-import java.awt.Rectangle;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.Timer;
+import javax.swing.*;
+import javax.swing.ImageIcon;
 
 import Simulation.Clock;
 import Simulation.SandwichTruck;
@@ -13,7 +13,10 @@ import Simulation.SandwichTruck;
 /**
  * Creates JFrame, adds GridPanel
  * 
- * @author Daniel
+ * @author Daniel ,
+ *
+ * add askUser method to ask the user which strategy will be used.
+ * @author Riyad
  */
 public class MapWindow extends JFrame {
 	/**
@@ -26,17 +29,19 @@ public class MapWindow extends JFrame {
 	public MapWindow(SandwichTruck truck) {
 		this.truck = truck;
 		this.hoodSize = new Rectangle(20, 20);
+		askUser();
 		init();
 	}
 
 	public MapWindow(SandwichTruck truck, Rectangle neighborHoodSize) {
 		this.truck = truck;
 		this.hoodSize = neighborHoodSize;
+		askUser();
 		init();
 	}
 
 	private void init() {
-		setSize(420, 420);
+		setSize(400, 400);
 		setTitle("Sandwich Truck Simulation");
 		GridPanel grid = new GridPanel(truck, hoodSize);
 		grid.setSize(400, 400);
@@ -54,5 +59,45 @@ public class MapWindow extends JFrame {
 
 		repaintTimer.setDelay(100);
 		repaintTimer.start();
+	}
+
+	private void askUser() {
+
+		String userInputStr;
+		//int userInput;
+		ImageIcon iconSch = new ImageIcon("Simulation/SchaperLogo2.png");
+
+		while (true) {
+//			userInputStr = JOptionPane.showInputDialog(null, "Choose a strategy movement for the truck: " +
+//					" \n1: both left and right hand turns"+
+//					"\n 2: only left hand turn", "Strategy Input", JOptionPane.QUESTION_MESSAGE);
+			Object[] strategies = {"","both-hand turn", "left-hand turn"};
+			userInputStr = (String) JOptionPane.showInputDialog(null,
+					"Choose a strategy movement for the truck: ",
+					"Select Strategy",
+					JOptionPane.PLAIN_MESSAGE,
+					iconSch,
+					strategies,
+					"");
+			try {
+				//userInput = Integer.parseInt(userInputStr);
+				if (userInputStr.contains("both")){
+					truck.selectedStrategy=1;
+				}
+				else if (userInputStr.contains("left")){
+					truck.selectedStrategy=2;
+				}
+				else
+					throw new RuntimeException("Not a valid choice!");
+				break;
+			}
+//			catch(NumberFormatException ex) {
+//				JOptionPane.showMessageDialog(null, "Choose a number!", "Error Dialog", JOptionPane.ERROR_MESSAGE);
+//			}
+			catch(Exception ex) {
+				JOptionPane.showMessageDialog(null, ex.getMessage(), "Error Dialog", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		JOptionPane.showMessageDialog(null, "You chose a  " + userInputStr +" strategy \n Press OK to start delivering", "Result", JOptionPane.INFORMATION_MESSAGE,iconSch);
 	}
 }
